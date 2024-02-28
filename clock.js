2a14bbbe57f0fd27828f015044795debb2630c6c
satork <sebastian.kedzier@gmail.com>
2024-02-28 17:25:20 +0100

    refactor from local

--- clock.js
+++ clock.js
@@ -16,10 +16,17 @@ class Clock {
         ctx.arc(0, 0, radius, 0, 2 * Math.PI);
         ctx.lineWidth = this.width;
         ctx.strokeStyle = this.color;
-        ctx.fillStyle = "black"
         ctx.lineWidth = 2
         ctx.fill();
         ctx.stroke();
+        ctx.restore();
+    }
+    numbers(number,posX,posY){
+        ctx.save();
+        ctx.translate(150,150)
+        ctx.fillStyle = "#ffffff"
+        ctx.font = "36px Garamond";
+        ctx.fillText(number,posX,posY);
         ctx.restore();
     }
     tick(time, height){
@@ -36,7 +43,7 @@ class Clock {
     }
     tickClear(){
         ctx.beginPath();
-        ctx.arc(0, 0, 138, 0, 2 * Math.PI);
+        ctx.arc(0, 0, 128, 0, 2 * Math.PI);
         ctx.strokeStyle = this.color;
         ctx.fillStyle = this.color;
         ctx.lineWidth = 1
@@ -46,15 +53,16 @@ class Clock {
 }
 
 //DRAWING ANALOG CLOCK
-    const clockShield = new Clock("#ffffff", 5);
-    clockShield.shield(140);
+    const clockShield = new Clock("#fffffff", 1);
+    clockShield.shield(130);
+    const shieldNumber = new Clock("#ffffff", 5);
+
 
 //CREATING TICKS AND CLEARER
     const hoursTick = new Clock("#000000", 5);
     const minutesTick = new Clock("#000000", 5);
     const secondsTick = new Clock("#000000", 2);
-    const tickClear = new Clock("rgb(45, 93, 122, 1)", 1);
-    const innerCircle = new Clock("#000000", 1)
+    const tickClear = new Clock("#454d73", 1);
     const tickLenght = { minutes: 105, hours: 70, seconds: 125
     }
 
@@ -71,12 +79,12 @@ const clockTicking = () => {
     // ACTUAL DATE
     let date = new Date();
     let thisSeconds = date.getSeconds() + (Math.floor(date.getMilliseconds() / 100) / 10) ;
-    let seconds = thisSeconds
-    let thisHour = date.getHours();
-    let hour = thisHour * 5;
+    let seconds = thisSeconds;
     let thisMinutes = date.getMinutes();
     let minutes = thisMinutes;
-    
+    let thisHour = date.getHours();
+    let hour = thisHour * 5 + (minutes * 0.016 * 5);
+
     //DRAWING TICKS
     ctx.translate(150,150)
     tickClear.tickClear();
@@ -84,15 +92,27 @@ const clockTicking = () => {
     hoursTick.tick(hour, tickLenght.hours);
     secondsTick.tick(seconds, tickLenght.seconds);
     ctx.translate(-150, -150);
-    innerCircle.shield(3);
+    shieldNumber.numbers("1",50,-80);
+    shieldNumber.numbers("2",90,-45);
+    shieldNumber.numbers("3",105,10);
+    shieldNumber.numbers("4",90,60);
+    shieldNumber.numbers("5",50,100);
+    shieldNumber.numbers("6",-3,120);
+    shieldNumber.numbers("7",-60,100);
+    shieldNumber.numbers("8",-105,60);
+    shieldNumber.numbers("9",-120,10);
+    shieldNumber.numbers("10",-110,-45);
+    shieldNumber.numbers("11",-70,-80);
+    shieldNumber.numbers("12",-15,-95);
+
 
     //DRAWING DIGITAL
-    if (thisHour < 10) { digitalHour = `0${thisHour}`} else {digitalHour = thisHour}
-    if (thisMinutes < 10) { digitalMinutes = `0${thisMinutes}`} else {digitalMinutes = thisMinutes}
-    if (thisSeconds < 10) { digitalSeconds = `0${Math.floor(thisSeconds)}`} else {digitalSeconds = Math.floor(thisSeconds)}
+    // if (thisHour < 10) { digitalHour = `0${thisHour}`} else {digitalHour = thisHour}
+    // if (thisMinutes < 10) { digitalMinutes = `0${thisMinutes}`} else {digitalMinutes = thisMinutes}
+    // if (thisSeconds < 10) { digitalSeconds = `0${Math.floor(thisSeconds)}`} else {digitalSeconds = Math.floor(thisSeconds)}
 
-    digital.innerHTML = `${digitalHour} ${digitalSpace} ${digitalMinutes} ${digitalSpace} ${digitalSeconds}`
+    // digital.innerHTML = `${digitalHour} ${digitalSpace} ${digitalMinutes} ${digitalSpace} ${digitalSeconds}`
 }
 
-setInterval(clockTicking, 100);
+setInterval(clockTicking, 10);
 ctx.setTransform(1, 0, 0, 1, 0 ,0);
